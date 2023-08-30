232dccf74588bbfa62423ee388440d20e431567b
IsaakBM <ibrito@conservation.org>
2023-08-30 14:24:05 -0700

    Update TESTING_global_pus_update.R

--- zscripts/TESTING_global_pus_update.R
+++ zscripts/TESTING_global_pus_update.R
@@ -15,26 +15,31 @@ rs[] <- 1:ncell(rs)
 terra::rast(c(xmin = -180, xmax = 180, ymax = 90, ymin = -90), res = 1, crs = "+proj=robin +lon_0=0 +datum=WGS84 +units=m +no_defs")
 
 
+
+
+#
+library(terra)
+library(sf)
+library(rnaturalearth)
+LatLon <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" # nolint
+moll <- "+proj=moll +lon_0=0 +datum=WGS84 +units=m +no_defs" # nolint
+robin <- "+proj=robin +lon_0=0 +datum=WGS84 +units=m +no_defs" # nolint
+
+
+world_borders_sf <- ne_countries(scale = "medium", returnclass = "sf")
+world_borders_rs <- as(world_borders_sf, "SpatVector")
 rs <- terra::rast(ncol = 1440, nrow = 720, extent = ext(c(-180, 180, -90, 90)))
 rs[] <- 1:1036800
 
 b <- terra::rasterize(world_borders_rs, rs)
-plot(b)
-mean(b[], na.rm = TRUE)
-
 b[] <- ifelse(is.na(b[]), 2, NA)
-plot(b)
-b2 <- terra::project(b, y = moll)
-plot(b2)
+b2 <- terra::project(b, y = robin)
 
 c <- terra::as.polygons(b2)
-plot(c)
 c <- st_as_sf(c)
-plot(st_geometry(c))
-
 
 # Area
-CellArea <- 50 # in km2
+CellArea <- 10000 # in km2
 h_diameter <- 2 * sqrt((CellArea*1e6)/((3*sqrt(3)/2))) * sqrt(3)/2 # Diameter in m
 s_diameter <- sqrt(CellArea*1e6) # Diameter in m
 
